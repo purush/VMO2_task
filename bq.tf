3b82cca701428a2650260f82b267218513593329
purush <dabtesto2@gmai.com>
2023-04-13 11:42:19 +0100

    added local variable type map role_assignment_to_bq_data_set

--- bq.tf
+++ bq.tf
@@ -7,8 +7,19 @@ resource "google_bigquery_dataset" "vmo2_tech_test_bq" {
 }
 
 module "bq_access_data_editor" {
+  for_each = local.role_data_set_id
   source               = "./modules/bq_dataset_access"
-  bq_data_set_id       = google_bigquery_dataset.vmo2_tech_test_bq.dataset_id
-  bq_data_set_role     = "roles/bigquery.dataEditor"
-  bq_access_user_email = google_service_account.bq_editor_sa.email
+  bq_data_set_id       = each.value["id"]
+  bq_data_set_role     = each.value["role"]
+  bq_access_user_email = each.value["email"]
+}
+
+locals {
+  role_data_set_id = {
+    "sa-editor" = {
+    id = google_bigquery_dataset.vmo2_tech_test_bq.dataset_id 
+    role = "roles/bigquery.dataEditor"
+    email =  google_service_account.bq_editor_sa.email
+    }
+  }
 }
